d7f676ab6015c7b0a1e6918a24c749377ff74aa7
RavinduShenal <ravindushenal2004@gmail.com>
2025-12-22 13:29:53 +0530

    simple search

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
+import {useState} from "react";
 import propertiesData from './data/properties.json';
 import PropertyCard from './components/PropertyCard';
+import SearchForm from './components/SearchForm';
 
 function App() {
-  const properties = propertiesData.properties;
+  const allProperties = propertiesData.properties;
+  const [filteredProperties, setFilteredProperties] = useState(allProperties);
+
+  function handleSearch(criteria) {
+    const results = allProperties.filter(property => {
+      if (criteria.type && property.type !== criteria.type) {
+        return false;
+      }
+
+      if (criteria.minPrice && property.price < criteria.minPrice) {
+        return false;
+      }
+
+      if (criteria.maxPrice && property.price > criteria.maxPrice) {
+        return false;
+      }
+
+      return true;
+    });
+
+    setFilteredProperties(results);
+  }
 
   return (
     <div>
       <h1>Estate Agent App</h1>
 
-      {properties.map(property => (
+      <SearchForm onSearch={handleSearch}/>
+
+      {filteredProperties.map(property => (
         <PropertyCard key={property.id} property={property} />
       ))}
     </div>
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+function SearchForm({ onSearch }) {
+  const [type, setType] = useState("");
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    onSearch({
+      type,
+      minPrice,
+      maxPrice
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Property Type:
+        <select value={type} onChange={e => setType(e.target.value)}>
+          <option value="">Any</option>
+          <option value="House">House</option>
+          <option value="Flat">Flat</option>
+        </select>
+      </label>
+
+      <br /><br />
+
+      <label>
+        Min Price:
+        <input
+          type="number"
+          value={minPrice}
+          onChange={e => setMinPrice(e.target.value)}
+        />
+      </label>
+
+      <br /><br />
+
+      <label>
+        Max Price:
+        <input
+          type="number"
+          value={maxPrice}
+          onChange={e => setMaxPrice(e.target.value)}
+        />
+      </label>
+
+      <br /><br />
+
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default SearchForm;
