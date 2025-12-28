de32d131aa8943c323a3882d7b71c7843dbc7b34
RavinduShenal <ravindushenal2004@gmail.com>
2025-12-29 00:58:42 +0530

    add to fav button in property card

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,36 +1,35 @@
 import { Link } from "react-router-dom";
 import "../styles/PropertyCard.css";
 
-function PropertyCard({ property, favourites, toggleFavourite }) {
-
+function PropertyCard({ property, favourites, addFavourite }) {
   const isFavourite = favourites.includes(property.id);
 
   return (
-    <div className="property-card">
-
-      <button
-        type="button"
-        className={`fav-btn ${isFavourite ? "active" : ""}`}
-        onClick={(e) => {
-          e.stopPropagation();
-          e.preventDefault();
-          toggleFavourite(property.id);
-        }}
-      >
-        ♥
-      </button>
-
-      {/* Clickable card content */}
+    <div
+      className="property-card"
+      draggable
+      onDragStart={(e) =>
+        e.dataTransfer.setData("propertyId", property.id)
+      }
+    >
       <Link to={`/property/${property.id}`} className="property-link">
         <img src={property.picture} alt={property.type} />
 
         <div className="property-info">
           <h3>{property.type}</h3>
           <p>{property.location}</p>
-          <p><strong>£{property.price.toLocaleString()}</strong></p>
+          <p>£{property.price.toLocaleString()}</p>
         </div>
       </Link>
 
+      <button
+        type="button"
+        className="fav-btn"
+        disabled={isFavourite}
+        onClick={() => addFavourite(property.id)}
+      >
+        {isFavourite ? "Favourited" : "Add to Favourites"}
+      </button>
     </div>
   );
 }
