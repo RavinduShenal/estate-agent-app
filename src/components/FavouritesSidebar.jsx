28e99c0ea832753d3b91454b53a311d7fe0b6f46
RavinduShenal <ravindushenal2004@gmail.com>
2025-12-30 01:02:39 +0530

    Favourite Sidebar Styling

--- src/components/FavouritesSidebar.jsx
+++ src/components/FavouritesSidebar.jsx
@@ -17,22 +17,30 @@ function FavouritesSidebar({ favourites, properties, onRemove, onClear, onDrop }
       <h3>Favourites</h3>
 
       {favouriteProperties.length === 0 && (
-        <p>Drag properties here</p>
+        <p className="empty-message">Drag properties here</p>
       )}
-
-      {favouriteProperties.map(property => (
-        <div
-          key={property.id}
-          className="favourite-item"
-          draggable
-          onDragEnd={() => onRemove(property.id)}
-        >
-          <span>
-            {property.type} – £{property.price.toLocaleString()}
-          </span>
-          <button onClick={() => onRemove(property.id)}>Remove</button>
-        </div>
-      ))}
+    
+    <div className="favourites-list">
+        {favouriteProperties.map(property => (
+            <div key={property.id} className="favourite-item" draggable
+            onDragEnd={() => onRemove(property.id)}
+            >
+            <img
+              src={property.pictures[0]}
+              alt={property.type}
+              className="favourite-image"
+            />
+            <div className="favourite-info">
+              <h4 className="favourite-location">{property.location}</h4>
+              <p className="favourite-price">£{property.price.toLocaleString()}</p>
+              <p className="favourite-type">{property.bedrooms} bed {property.type}</p>
+              <button className="remove-btn" onClick={() => onRemove(property.id)}>
+                Remove
+              </button>
+            </div>
+        </div>    
+        ))}
+    </div>
 
       {favouriteProperties.length > 0 && (
         <button className="clear-btn" onClick={onClear}>
